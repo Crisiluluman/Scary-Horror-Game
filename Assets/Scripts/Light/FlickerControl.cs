2df7d62c1d0808928560f78bfb586f4e2f337da8
Crisiluluman <kawazu-maner@hotmail.com>
2022-04-26 21:14:01 +0200

    Adding more stuff
    Need to push to make space for new asset files, as github only lets you upload 100mb

---
+++ Assets/Scripts/Light/FlickerControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerControl : MonoBehaviour
+{
+    [SerializeField] private float minFlicker = 0.01f;
+    [SerializeField] private float maxFlicker = 0.2f;
+
+    public bool isFlickering = false;
+    public float timeDelay;
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight() {
+        isFlickering = true;
+
+        //Randomizes amount of flickeirng 
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(minFlicker, maxFlicker);
+
+        //Returns the flicker value
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        
+        //Same as before, but adds another layer of randomness
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+
+        isFlickering = false;
+    }
+
+
+}
